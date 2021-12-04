5e9a189abadf19045c3fef619db3c9dc78996eca
Pat Viafore <patviafore@gmail.com>
2021-12-03 21:49:22 -0600

    Challenge 3

--- test/test_challenges.py
+++ test/test_challenges.py
@@ -7,3 +7,8 @@ def test_challenge2():
     from challenge2 import MOVES, get_position_value, get_improved_position_value
     assert get_position_value(MOVES) == 1451208
     assert get_improved_position_value(MOVES) == 1620141160
+
+def test_challenge3():
+    from challenge3 import NUMBERS, get_power_rate, get_life_support_rating
+    assert get_power_rate(NUMBERS) == 3320834
+    assert get_life_support_rating(NUMBERS) == 4481199
