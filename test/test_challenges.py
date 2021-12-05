6cdbef1153f8ea7bff7f29189649a9f3843cec4c
Pat Viafore <patviafore@gmail.com>
2021-12-05 10:05:52 -0600

    Add challenge 5

--- test/test_challenges.py
+++ test/test_challenges.py
@@ -12,3 +12,13 @@ def test_challenge3():
     from challenge3 import NUMBERS, get_power_rate, get_life_support_rating
     assert get_power_rate(NUMBERS) == 3320834
     assert get_life_support_rating(NUMBERS) == 4481199
+
+def test_challenge4():
+    from challenge4 import GAME, get_winning_board_score, get_losing_board_score
+    assert get_winning_board_score(GAME) == 51776
+    assert get_losing_board_score(GAME) == 16830
+
+def test_challenge5():
+    from challenge5 import LINES, get_number_of_overlapping_points, get_number_of_overlapping_points_no_diagonal
+    assert get_number_of_overlapping_points_no_diagonal(LINES) == 5147
+    assert get_number_of_overlapping_points(LINES) == 16925
