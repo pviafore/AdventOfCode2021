5027c9a6488f5025c955a60ccaf32f367a583506
Pat Viafore <patviafore@gmail.com>
2021-12-18 21:07:38 -0600

    Challenge 17

---
+++ challenge17.py
@@ -0,0 +1,71 @@
+import itertools
+import re
+
+from functools import lru_cache
+from typing import Generator
+
+Target = tuple[int, int, int, int]
+def to_target(text: str) -> Target:
+    parts = re.split(r'(?:target area: x=)|(?:\.\.)|(?:, y=)', text)
+    return (int(parts[1]), int(parts[2]), int(parts[3]), int(parts[4]))
+
+# only good for positive y-velocities
+def get_highest_y(target:  Target) -> int:
+    upper_bound = abs(target[2])
+    return upper_bound * (upper_bound -1) // 2
+
+def get_total_number_of_shots(target: Target) -> int:
+    values = set()
+    points = itertools.product(range(get_first_x_value(target), target[1]+1),
+                               range(target[2], -1*target[2] + 1))
+
+    new_values = set()
+    for x,y in points:
+        total_x = 0
+        total_y = 0
+        x_step = x
+        y_step = y
+        while total_x <= target[1] and total_y >= target[2]:
+            total_x += x_step
+            total_y += y_step
+            if x_step > 0:
+                x_step -=1
+            y_step -= 1
+            if target[0] <= total_x <= target[1] and target[2] <= total_y <= target[3]:
+                new_values.add((x,y))
+
+    return len(new_values)
+
+def get_y_values(target: Target, steps: int):
+    y_values = []
+    max_y = (steps - 1) // 2 # at this point, we are at zero
+    for y in range(max_y, target[2]-1, -1):
+        # these are the ys to check
+        final_y = get_y_value(y, steps)
+        if target[2] <= final_y <= target[3]:
+            y_values.append(y)
+    return y_values
+
+@lru_cache
+def get_y_value(initial_y: int, steps: int):
+    total_so_far = 0
+    for step in range(steps):
+       total_so_far += initial_y
+       initial_y -= 1
+    return total_so_far
+
+
+def accumulator() -> Generator[tuple[int, int], None, None]:
+    yield from zip(itertools.count(start=1), itertools.accumulate(itertools.count(start=1)))
+
+
+def get_first_x_value(target: Target) -> int:
+    return next(i for i,sum in accumulator() if sum >= target[0])
+
+
+with open("input/input17.txt") as f:
+    TARGET = to_target(f.read().strip())
+
+if __name__ == "__main__":
+    print(f"Highest y: {get_highest_y(TARGET)}")
+    print(f"Possible Values: {get_total_number_of_shots(TARGET)}")
